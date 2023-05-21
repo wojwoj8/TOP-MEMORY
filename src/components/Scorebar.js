f3c13030a7b88343fac70ad7807efbdee0893a83
wojwoj8 <wojwoj8@gmail.com>
2023-05-21 12:33:35 +0200

    scoreboard scss

--- src/components/Scorebar.js
+++ src/components/Scorebar.js
@@ -2,15 +2,19 @@ const Scoreboard = () =>{
 
     return(
         <div className="scoreboard-content">
-            <div className="scoreboard-title">
-                <h2>
-                    Programming Languages Memory game
-                </h2>
-            </div>
-            <div className="scoreboard">
-                
-            </div>
+            <div className="scoreboard-in">
 
+            
+                <div className="scoreboard-title">
+                    <h2>
+                        Programming Languages Memory game
+                    </h2>
+                </div>
+                <div className="scoreboard">
+                    <p className="score">Score: </p>
+                    <p className="high-score">High Score: </p>
+                </div>
+            </div>
         </div>
     )
 
